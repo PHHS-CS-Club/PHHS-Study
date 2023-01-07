63cdf238c6e9c6b5eb5654c98923c4560a7f0510
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-01-06 20:45:29 -0800

    sign in changes

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,32 +2,33 @@ import React, { useEffect } from "react";
 import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import "./Signin.css";
 
 export default function Signin() {
-	const { googleSignIn, user } = UserAuth();
-	const navigate = useNavigate();
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
 
-	const handleGoogleSignIn = async () => {
-		try {
-			await googleSignIn();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		if (user !== null) {
-			navigate("/Account");
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user]);
+  useEffect(() => {
+    if (user !== null) {
+      navigate("/Account");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
-	return (
-		<>
-			<div>
-				<div>Sign In</div>
-				<GoogleButton onClick={handleGoogleSignIn} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div class="signin-total">
+        <div class="signin-text">Sign In Below</div>
+        <GoogleButton class="sign-google-link" onClick={handleGoogleSignIn} />
+      </div>
+    </>
+  );
 }
