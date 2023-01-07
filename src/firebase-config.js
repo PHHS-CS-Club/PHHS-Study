0020799bb8184db23af78adc7be36441c87474f1
Ryan Virtue <113137446+RyanVSD@users.noreply.github.com>
2023-01-06 16:31:14 -0800

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,21 +3,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const apiKey = process.env.REACT_APP_API_KEY;
-console.log(apiKey);
 const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
-console.log(authDomain);
 const databaseURL = process.env.REACT_APP_DATABASE_URL;
-console.log(databaseURL);
 const projectId = process.env.REACT_APP_PROJECT_ID;
-console.log(projectId);
 const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
-console.log(storageBucket);
 const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
-console.log(messagingSenderId);
 const appId = process.env.REACT_APP_APP_ID;
-console.log(appId);
 const measurementId = process.env.REACT_APP_MEASUREMENT_ID;
-console.log(measurementId);
 
 const firebaseConfig = {
 	apiKey: apiKey,
