388c6c78709f42f63a6a99eb7d0630017af2f523
OliverCushman <oliver.cushman@mrjaffesclass.com>
2023-02-10 13:09:55 -0800

    added user fields

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,17 +2,19 @@ import React, { useEffect } from "react";
 import { set, ref, onValue } from "firebase/database";
 import { database } from "../firebase-config";
 import { UserAuth } from "../context/AuthContext";
+import { useParams } from "react-router-dom";
 import "./Account.css";
 
 export default function Signin() {
+  let { id } = useParams();
   const { user, logOut } = UserAuth();
   useEffect(() => {
     //Updates user's data when opening account page
-    if (user.uid !== undefined) {
-      const dbRef = ref(database, "users/" + user.uid);
+    if (id !== undefined) {
+      const dbRef = ref(database, "users/" + id);
       onValue(dbRef, (snapshot) => {
         const data = snapshot.val();
-        if (!data) {
+        if (!data && id === user.uid) {
           set(dbRef, {
             username: user.displayName,
             email: user.email,
@@ -21,7 +23,7 @@ export default function Signin() {
         }
       });
     }
-  }, [user]);
+  }, [id, user]);
 
   return (
     //display any user information from signed in user here
@@ -30,19 +32,42 @@ export default function Signin() {
     //current database structure has each account with a role, email, and display name
     //all of which are accessed through user/ with their user id
     <>
-      <div class="account-starter">Account Page Dev Test Addition 2</div>
-      <p>
-        username:
-      </p>
-      <p>
-        email:
-      </p>
-      <p>
-        most popular decks:
-      </p>
-      <button class="account-logout" onClick={logOut}>
+      <div className="account-starter">Account Page Dev Test Addition 2</div>
+      <div className="account-info">
+        <div className="user-field">
+          <p className="field-text">
+            Username: {getUsername(user.uid)}
+          </p>
+          <button className="change-field" onClick={() => changeUsername(user.uid, "normal")}>
+            Change username
+          </button>
+        </div>
+        <div className="user-field">
+          <p className="field-text">
+            Email: {user.email}
+          </p>
+        </div>
+      </div>
+      <button className="account-logout" onClick={logOut}>
         Log Out
       </button>
     </>
   );
+}
+
+const changeUsername = (userId, newName) => {
+  set(ref(database, "users/" + userId), {
+    username: newName,
+  });
+}
+
+const getUsername = (userId) => {
+  console.log("getting username...")
+  let name; 
+  const userRef = ref(database, "users/" + userId + "/username");
+  onValue(userRef, (snapshot) => {
+    name = snapshot.val();
+  });
+
+  return name;
 }
