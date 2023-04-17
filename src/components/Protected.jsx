d027255c9fb48ac5e7c1162a1815473007453c88
Ryan V <ryan.virtue@mrjaffesclass.com>
2023-04-16 17:40:11 -0700

    admin page

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import { onValue, ref } from "firebase/database";
+import { database } from "../firebase-config";
+import { useEffect } from "react";
+
+export const AdminLevelProtected = ({ children }) => {
+  const { user } = UserAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onValue(
+      ref(database, "users/" + user.uid + "/role"),
+      (snapshot) => {
+        if (snapshot.val() !== "Admin" && snapshot.val() != null) {
+          navigate("/Home");
+        }
+      },
+      {
+        onlyOnce: true,
+      }
+    );
+  }, [children, navigate, user.uid]);
+  if (user?.uid !== null && user?.uid !== undefined) {
+    return children;
+  }
+};
 
 const Protected = ({ children }) => {
-	const { user } = UserAuth();
-	if (!user) {
-		return <Navigate to="/Home" />;
-	}
+  const { user } = UserAuth();
+  if (!user) {
+    return <Navigate to="/Home" />;
+  }
 
-	return children;
+  return children;
 };
 
 export default Protected;
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Admin() {
+  console.log("admin");
+  return (
+    <>
+      <div>Admin page</div>
+    </>
+  );
+}
