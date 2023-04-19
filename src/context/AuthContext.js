c472ae7d6fd808332b746ec49225825cab92d92d
Ryan V <ryan.virtue@mrjaffesclass.com>
2023-04-19 16:26:13 -0700

    added comments to components i've wrote

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,45 +1,47 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import {
-	/* eslint-disable no-unused-vars */
-	GoogleAuthProvider,
-	signInWithRedirect,
-	signOut,
-	onAuthStateChanged,
-	/* eslint-enable no-unused-vars */
+  /* eslint-disable no-unused-vars */
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+  /* eslint-enable no-unused-vars */
 } from "firebase/auth";
 
 import { auth } from "../firebase-config";
 
 const AuthContext = createContext();
 
+//provides the user, signin function, logout function across the website.
+//access them by importing { UserAuth }, and then simply write const { user } = UserAuth(); and the user will have the signed in user's information
 export const AuthContextProvider = ({ children }) => {
-	const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
-	const googleSignIn = () => {
-		const provider = new GoogleAuthProvider();
-		signInWithRedirect(auth, provider);
-	};
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
 
-	const logOut = () => {
-		signOut(auth);
-	};
+  const logOut = () => {
+    signOut(auth);
+  };
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-			setUser(currentUser);
-		});
-		return () => {
-			unsubscribe();
-		};
-	}, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-	return (
-		<AuthContext.Provider value={{ googleSignIn, logOut, user }}>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 };
