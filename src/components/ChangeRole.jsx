fe6520f76409e68808d69b723a2026a62f1d821f
OliverCushman <oliver.cushman@mrjaffesclass.com>
2023-04-28 12:54:27 -0700

    added doc to  components

--- src/components/ChangeRole.jsx
+++ src/components/ChangeRole.jsx
@@ -6,13 +6,20 @@ import "./ChangeUserField.css";
 
 export default function ChangeRole() {
   let { id } = useParams();
+
   const dbRef = ref(database, "users/" + id);
+
   const [roleInput, setRoleInput] = useState("");
 
+  // Sets roleInput to the current value of the select element
+  // Called when value of select element changes
   const changeRoleInput = (event) => {
     setRoleInput(event.target.value);
   };
 
+  // Changes the role of the user to roleInput if the user has selected a role in the select element
+  // Otherwise alerts
+  // Called when user presses 'Change role' button
   const changeRole = () => {
     if (roleInput !== "") {
       update(dbRef, {
@@ -23,6 +30,8 @@ export default function ChangeRole() {
     }
   };
 
+  // Select element with possible roles
+  // Button that changes the user's role to the selected role if there is one selected
   return (
     <div>
     	<label htmlFor="role-select" className="user-field-change-label">
