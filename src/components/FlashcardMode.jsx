6b7253c310d17161243725b3fdcf983c04c3a72a
Ryan Virtue <ryan.virtue@mrjaffesclass.com>
2023-04-28 13:23:45 -0700

    added more comments, removed some unessecary code

--- src/components/FlashcardMode.jsx
+++ src/components/FlashcardMode.jsx
@@ -25,28 +25,39 @@ export default function FlashcardMode(props) {
     //eslint-disable-next-line
   }, []);
 
+  //Initializes the cards
   function initial() {
+    //If there is a user it will attempt to get user data
     if (user !== null && user !== undefined) {
       onValue(
         ref(database, "users/" + user.uid + "/" + id),
         (snapshot) => {
+          //If it gets null data then it will default set it
           if (snapshot.val() !== null && snapshot.val() !== undefined) {
+            //Gets array data
             var arr = snapshot.val();
+            //Sets each card to the card object with their bucket and index
             arr.forEach((c, i) => {
               arr[i] = { ...props.cards[i], bucket: c.bucket, index: i };
             });
+            //Sets cards
             setCards(arr);
             let keys = Object.keys(snapshot.val());
+            //Sets current to a random card
             setCurrentCard(
               snapshot.val()[keys[Math.floor(Math.random() * keys.length)]]
             );
           } else {
+            //Sets cards
             arr = props.cards;
+            //Sets buckets and index
             arr.forEach((c, i) => {
               arr[i] = { ...c, bucket: 1, index: i };
             });
+            //Sets cards
             setCards(arr);
             let keys = Object.keys(arr);
+            //Sets current to a random card
             setCurrentCard(arr[keys[Math.floor(Math.random() * keys.length)]]);
           }
         },
@@ -55,36 +66,47 @@ export default function FlashcardMode(props) {
         }
       );
     } else {
+      //Gets cards and sets all to bucket 1
       var arr = props.cards;
       arr.forEach((c, i) => {
         arr[i] = { ...c, bucket: 1, index: i };
       });
+      //Sets cards
       setCards(arr);
       let keys = Object.keys(arr);
+      //Sets current to a random card
       setCurrentCard(arr[keys[Math.floor(Math.random() * keys.length)]]);
     }
   }
 
+  //Sets current
   function getNewCard() {
+    //Filters out the current card and bucket
     let arr = cards.filter((x) => {
       return x.id !== currentCard.id && x.bucket === currentBucket;
     });
     let i = 0;
-    let tempBucket;
+    //Runs until there is options for the array to pick from or 100 iterations
     while (arr.length < 1 && i < 100) {
-      let newBucket = pickBucket(false, tempBucket);
-      if (tempBucket !== newBucket) newBucket = tempBucket;
+      //Gets a new bucket
+      let newBucket = pickBucket(false);
+      //Filters cards
       arr = cards.filter((x) => {
         return x.id !== currentCard.id && x.bucket === newBucket;
       });
       i++;
-      tempBucket = pickBucket(false, newBucket);
     }
+    //Sets current card to a random from the available cards
     setCurrentCard(arr[Math.floor(Math.random() * arr.length)]);
   }
 
+  //picks a new bucket
+  //Parameters: 
+  //notSame (boolean): if true it will not pick the same bucket as the notBucket int
+  //notBucket (int): must be inputted if notSame is true
   function pickBucket(notSame, notBucket) {
     let buckets = [0, 0, 0, 0, 0];
+    //Increments the index matching the bucket num
     cards.forEach((x) => {
       for (let i = 0; i < buckets.length; i++) {
         if (x.bucket === i + 1) {
@@ -92,6 +114,7 @@ export default function FlashcardMode(props) {
         }
       }
     });
+    //If a bucket has any cards, it recieves a weight
     let weights = [
       buckets[0] > 0 ? 400 : 0,
       buckets[1] > 0 ? 50 : 0,
@@ -99,11 +122,14 @@ export default function FlashcardMode(props) {
       buckets[3] > 0 ? 3.125 : 0,
       buckets[4] > 0 ? 1 : 0,
     ];
+    //If not same then the bucket passed is unweighted
     if (notSame) weights[notBucket - 1] = 0;
     let sum = 0;
+    //Adds up weights
     for (let i = 0; i < weights.length; i++) {
       sum += weights[i];
     }
+    //Gets random num and decides which bucket num
     let random = Math.floor(Math.random() * sum);
     for (let i = 0; i < weights.length; i++) {
       if (random < weights[i]) {
@@ -113,8 +139,10 @@ export default function FlashcardMode(props) {
     }
   }
 
+  //When a correct card is picked this is ran
   const handleCorrect = () => {
     var arr = cards;
+    //Updates bucket num by +1
     for (let i = 0; i < arr.length; i++) {
       if (arr[i].id === currentCard.id) {
         if (arr[i].bucket < 5) {
@@ -122,12 +150,13 @@ export default function FlashcardMode(props) {
         }
       }
     }
-
+    //Gets new card and unflips the card
     setCards(arr);
     setCurrentBucket(pickBucket(false));
     getNewCard();
     setFlipped(!flipped);
     cardRef.current.setFlipped(!flipped);
+    //Updates the database with the new info
     if (
       user !== null &&
       user !== undefined &&
@@ -138,8 +167,10 @@ export default function FlashcardMode(props) {
     }
   };
 
+  //When a card is marked as wrong this is ran
   const handleIncorrect = () => {
     var arr = cards;
+    //Updates bucket
     arr.forEach((c, i) => {
       if (c.id === currentCard.id) {
         if (c.bucket > 1) {
@@ -147,11 +178,13 @@ export default function FlashcardMode(props) {
         }
       }
     });
+    //Gets new card
     setCards(arr);
     setCurrentBucket(pickBucket(false));
     getNewCard();
     setFlipped(!flipped);
     cardRef.current.setFlipped(!flipped);
+    //Updates database
     if (
       user !== null &&
       user !== undefined &&
@@ -162,8 +195,11 @@ export default function FlashcardMode(props) {
     }
   };
 
+  //Deletes progress
   const resetProgress = () => {
-    remove(ref(database, "users/" + user.uid + "/" + id));
+    if(user?.displayName !== undefined) {
+      remove(ref(database, "users/" + user.uid + "/" + id));
+    }
     initial();
   };
 
