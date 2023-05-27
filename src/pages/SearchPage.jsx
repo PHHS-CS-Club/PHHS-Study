8f41e012a9a18f25eb3facc8813aa8590c107b96
CharlieHavens80654 <charles.havens@mrjaffesclass.com>
2023-05-27 16:48:36 -0700

    Update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -5,9 +5,16 @@ import { ref, onValue } from "firebase/database";
 import { Link } from "react-router-dom";
 import SetBoxView from "../components/SetBoxView";
 import "./SearchPage.css";
+import ClassesMenu from "../components/ClassesMenu";
+import TeachersMenu from "../components/TeachersMenu";
 
 export default function SearchPage() {
   const [flashcardMeta, setFlashcardMeta] = useState({});
+  const [classes, setClasses] = useState({});
+  const [teachers, setTeachers] = useState({});
+  const [searchRecentInput, setSearchRecentInput] = useState("");
+  //useState to store search information
+  //useState object to store picked classes/teachers
   useEffect(() => {
     onValue(ref(database, "flashcard-sets/"), (snapshot) => {
       const data = snapshot.val();
@@ -16,20 +23,48 @@ export default function SearchPage() {
     // eslint-disable-next-line
   }, []);
 
+  function searchBar() {
+    return (
+      <div className="search-bar-container">
+        <input
+          key="search"
+          type="text"
+          placeholder="Search flashcards by title"
+          className="search-bar"
+          onChange={(e) => setSearchRecentInput(e.target.value.toLowerCase())}
+        />
+      </div>
+    );
+  }
+
   if (flashcardMeta !== undefined && flashcardMeta !== null) {
     return (
       <div className="search-page">
-        {Object.keys(flashcardMeta).map((key, index) => (
-          <div key={key} className="search-container">
-            {flashcardMeta[key].Name.length !== 0 ? (
-              <Link to={"/Set/" + key}>
-                <SetBoxView id={key} key={key} />
-              </Link>
-            ) : (
-              <Link to={"/Set/" + key}>{"No Title"}</Link>
-            )}
-          </div>
-        ))}
+        {searchBar()}
+        <div className="create-set-extras">
+          <ClassesMenu classSelect={(classes) => setClasses(classes)} />
+          <TeachersMenu teacherSelect={(teachers) => setTeachers(teachers)} />
+        </div>
+        {/**Input object onChange updates the search information in the useState */}
+        {/**Input object onChange updates the search information in the useState */}
+        {/**Before map filter the array using .filter().map where in the filter put some booleans to check if it matches*/}
+        {Object.keys(flashcardMeta)
+          .filter((item) => {
+            return flashcardMeta[item]?.Name.toLowerCase().includes(
+              searchRecentInput
+            );
+          })
+          .map((key, index) => (
+            <div key={key} className="search-cnpontainer">
+              {flashcardMeta[key].Name.length !== 0 ? (
+                <Link to={"/Set/" + key}>
+                  <SetBoxView id={key} key={key} />
+                </Link>
+              ) : (
+                <Link to={"/Set/" + key}>{"No Title"}</Link>
+              )}
+            </div>
+          ))}
       </div>
     );
   }
