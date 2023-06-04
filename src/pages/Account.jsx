62f45911c69ba1853e853f674e8767788c3623d6
RyanVirtue619 <113137954+RyanVirtue619@users.noreply.github.com>
2023-06-04 15:58:28 -0700

    fixed link bug and flashcard mode bugs

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -19,7 +19,7 @@ export default function Signin() {
   const [role, setRole] = useState("");
   const [email, setEmail] = useState("");
   const [madeSets, setMadeSets] = useState([]);
-  
+
   const [changingUsername, setChangingUsername] = useState(false);
   const [changingRole, setChangingRole] = useState(false);
 
@@ -28,27 +28,26 @@ export default function Signin() {
   useEffect(() => {
     //Updates user's data when opening account page
     if (id !== undefined && id === user.uid) {
-      onValue(
-        dbRef,
-        (snapshot) => {
-          setUsername(snapshot.child("username").val());
-          setRole(snapshot.child("role").val());
-          setEmail(snapshot.child("email").val());
-          setMadeSets(snapshot.child("madeSets").val());
-          //If user data is null, fills in user data to register user in database
-          if (snapshot.child("username").val() === null && 
+      onValue(dbRef, (snapshot) => {
+        setUsername(snapshot.child("username").val());
+        setRole(snapshot.child("role").val());
+        setEmail(snapshot.child("email").val());
+        setMadeSets(snapshot.child("madeSets").val());
+        //If user data is null, fills in user data to register user in database
+        if (
+          snapshot.child("username").val() === null &&
           snapshot.child("role").val() === null &&
           snapshot.child("email").val() === null &&
-          snapshot.child("madeSets").val() === null) {
-            set(dbRef, {
-              username: user.displayName,
-              email: user.email,
-              role: "Student",
-              madeSets: [],
-            });
-          }
+          snapshot.child("madeSets").val() === null
+        ) {
+          set(dbRef, {
+            username: user.displayName,
+            email: user.email,
+            role: "Student",
+            madeSets: [],
+          });
         }
-      );
+      });
     }
     //eslint-disable-next-line
   }, []);
@@ -63,9 +62,7 @@ export default function Signin() {
             {madeSets.map((key, index) => (
               <div key={key} className="search-container">
                 {madeSets[index].length !== 0 ? (
-                  <Link to={"/Set/" + key}>
-                    <SetBoxView id={key} key={key} />
-                  </Link>
+                  <SetBoxView id={key} key={key} />
                 ) : (
                   <Link to={"/Set/" + key}>{"No Title"}</Link>
                 )}
@@ -88,48 +85,56 @@ export default function Signin() {
   //Opens or closes the ui for changing username upon pressing pencil button
   const changeUsernameSwitch = () => {
     setChangingUsername(!changingUsername);
-  }
+  };
 
   //If the user clicks the pencil button and sets changingUsername to true, it shows the ui for changing username
   //If the user doesn't press it or presses the x to close it, it shows nothing
-  const changeUsernameDisplay = () =>  {
+  const changeUsernameDisplay = () => {
     if (changingUsername) {
-      return (
-        <ChangeUsername></ChangeUsername>
-      );
+      return <ChangeUsername></ChangeUsername>;
     }
-    return (
-      <></>
-    );
-  }
+    return <></>;
+  };
 
   //Opens or closes the ui for changing role upon pressing pencil button
   const changeRoleSwitch = () => {
     setChangingRole(!changingRole);
-  }
+  };
 
   //If the user clicks the pencil button and sets changingRole to true, it shows the ui for changing role
   //If the user doesn't press it or presses it again to close it, it shows nothing
-  const changeRoleDisplay = () =>  {
+  const changeRoleDisplay = () => {
     if (changingRole) {
-      return (
-        <ChangeRole></ChangeRole>
-      );
+      return <ChangeRole></ChangeRole>;
     }
-    return (
-      <></>
-    );
-  }
+    return <></>;
+  };
 
   //Changes the icon for opening/closing the ui for changing a user field
   //If the ui is not open, shows a pencil icon
   //If the button is pressed and the ui is opened, shows an x icon
   const changeFieldDisplayIcon = (changingField, changeField) => {
     if (!changingField) {
-      return (<img src={EditButton} className="edit-pencil" alt={changeField} width="12" height="12"/>)
+      return (
+        <img
+          src={EditButton}
+          className="edit-pencil"
+          alt={changeField}
+          width="12"
+          height="12"
+        />
+      );
     }
-    return (<img src={XButton} className="x-button" alt="Close" width="12" height="12"/>)
-  }
+    return (
+      <img
+        src={XButton}
+        className="x-button"
+        alt="Close"
+        width="12"
+        height="12"
+      />
+    );
+  };
 
   //Shows user's name, role, and email with the option to change name and role through an expandable ui
   return (
@@ -139,7 +144,10 @@ export default function Signin() {
         <div className="account-info">
           <div className="user-field">
             <div className="field-text">Username: {username}</div>
-            <button onClick={changeUsernameSwitch} className="change-field-button">
+            <button
+              onClick={changeUsernameSwitch}
+              className="change-field-button"
+            >
               {changeFieldDisplayIcon(changingUsername, "Change username")}
             </button>
             {changeUsernameDisplay()}
